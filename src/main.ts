e8ad9462a25abdce07a9a95556d5210aac7752f7
ashish kumar singh <ashishkumarsinghh@outlook.com>
2020-10-04 20:04:54 +0530

    change access control

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({ origin: ['https://calendash.vercel.app/', '*'] });
+  app.enableCors({ origin: '*' });
   const options = new DocumentBuilder()
     .setTitle('Calendash')
     .setDescription('Calendash API Description')
